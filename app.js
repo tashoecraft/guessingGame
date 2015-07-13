53ecc71a49862f7e53b6933d452dc36ac9640d44
Austin <tashoecraft@gmail.com>
2015-07-13 16:43:58 -0400

    Final, working game

--- app.js
+++ app.js
@@ -1,134 +1,144 @@
-//When a game begins, there should be a random number generated between 1-100.
+//When a Game begins, there should be a random number generated between 1-100.
 
-//Game object will hold all game related methods and variables
-var game = {};
+//Game object will hold all Game related methods and variables
 
-game.randomNumber = function() {
-  return Math.floor(Math.random()*100);
+function Game() {
+  this.guesses = [];
+  this.numberOfGuesses = 5;
+  this.numberToGuess = this.randomNumber();
 }
 
-game.numberToGuess = game.randomNumber();
-
-game.numberOfGuesses = 5;
-
-//Store all of the guesses and create a way to check if the guess is a repeat.
-game.guesses = [];
+Game.prototype.randomNumber = function() {
+  return Math.floor(Math.random()*100);
+};
 
 
-//Allow the user to guess only a certain amount of times. When they run out of guesses let them know the game is over.
-game.turnsLeft = function() {
-  if(numberOfGuesses === 0) {
-    //output: "Game Over, the number was 'number', click new game to play again"
+Game.prototype.output = function(string) {
+  $("#outputText").text(string);
+  $("#guessLeft").text("Guesses left: " + this.numberOfGuesses);
+  $("#guesses").text("Guessed already: " + this.guesses);
+};
+
+//Allow the user to guess only a certain amount of times. When they run out of guesses let them know the Game is over.
+Game.prototype.turnsLeft = function() {
+  if(this.numberOfGuesses === 0) {
+    this.output("The game is over, click play again to try a new game");
     return false;
   } else return true;
-}
+};
 
 //After the user submits a guess, indicate whether their guess is 'hot' or 'cold'. Let the user know if they need to guess higher or lower.
-game.checkGuess = function(number) {
-  if(number === game.numberToGuess) {
-    //output: "You got it!"
-    game.numberOfGuesses = 0;
+
+Game.prototype.checkGuess = function(number) {
+  if(number === this.numberToGuess) {
+    this.output("you got it!");
+    this.numberOfGuesses = 0;
   } else {
-    switch(number) {
-      case number < (game.numberToGuess-95):
-        //output: "You're a glacier, not even sure how you guessed so wrong. Guess way way way higher"
-        break;
-      case number > (game.numberToGuess+95)
-        //output: "You're a glacier, not even sure how you guessed so wrong. Guess way way way lower"
-        break;
-      case number < (game.numberToGuess-75)
-        //output: "You're nearly frozen, Guess alot higher"
-        break;
-      case number > (game.numberToGuess+75):
-        //output: "You're nearly frozen, Guess alot lower"
-        break;
-      case number < (game.numberToGuess-50):
-        //output: "You're pretty darn cold, Guess a good bit higher";
-        break;
-      case number > (game.numberToGuess+50)
-        //output: "You're pretty darn cold, Guess a good bit lower";
-        break;
-      case number < (game.numberToGuess - 35);
-        //output: "You're cold, Guess higher";
-        break;
-      case number < (game.numberToGuess + 35);
-          //output: "You're cold, Guess lower";
-          break;
-      case number < (game.numberToGuess - 20);
-          //output: "You're warming up, Guess a bit lower";
-          break;
-      case number < (game.numberToGuess + 20);
-          //output: "You're warnming up, Guess a bit higher";
-          break;
-      case number < (game.numberToGuess + 10);
-          //output: "You're hot, Guess a little lower";
-          break;
-      case number < (game.numberToGuess - 10);
-          //output: "You're hot, Guess a little higher";
-          break;
-      case number < (game.numberToGuess + 5);
-          //output: "You're smocking, Guess a small bit lower";
-          break;
-      case number < (game.numberToGuess - 5);
-          //output: "You're hot, Guess a small bit higher";
-          break;
-      case number < (game.numberToGuess + 2);
-          //output: "You're smocking, Guess a small bit lower";
-          break;
-      case number < (game.numberToGuess - 2);
-          //output: "You're smocking, Guess a small bit higher";
-          break;
-      case number === (game.numberToGuess + 1);
-          //output: "You're on fire!!! AHHHH, Call the fire deport and also guess a washington smaller";
-          break;
-      case number === (game.numberToGuess - 1);
-          //output: "You're on fire!!! AHHHH, Call the fire deport and also guess a washington smaller";
-          break;
-    }
+      if(number < (this.numberToGuess - 95))
+        this.output("You're a glacier, not even sure how you guessed so wrong. Guess way way way higher");
+
+      else if(number > (this.numberToGuess + 95))
+        this.output("You're a glacier, not even sure how you guessed so wrong. Guess way way way lower");
+
+      else if( number < (this.numberToGuess - 75))
+        this.output("You're nearly frozen, Guess alot higher");
+
+      else if( number > (this.numberToGuess + 75))
+        this.output("You're nearly frozen, Guess alot lower");
+
+      else if( number < (this.numberToGuess - 50))
+        this.output("You're pretty darn cold, Guess a good bit higher");
+
+      else if( number > (this.numberToGuess + 50))
+        this.output("You're pretty darn cold, Guess a good bit lower");
+
+      else if( number < (this.numberToGuess - 35))
+        this.output("You're cold, Guess higher");
+
+      else if( number > (this.numberToGuess + 35))
+          this.output("You're cold, Guess lower");
+
+      else if( number < (this.numberToGuess - 20))
+          this.output("You're warming up, Guess a bit higher");
+
+      else if( number > (this.numberToGuess + 20))
+          this.output("You're warnming up, Guess a bit lower");
+
+      else if( number < (this.numberToGuess - 10))
+          this.output("You're hot, Guess a little higher");
+
+      else if( number > (this.numberToGuess + 10))
+          this.output("You're hot, Guess a little lower");
+
+      else if( number < (this.numberToGuess - 5))
+          this.output("You're smoking, Guess a small bit higher");
+
+      else if( number > (this.numberToGuess + 5))
+          this.output("You're smoking, Guess a small bit lower");
+
+      else if( number < (this.numberToGuess - 2))
+          this.output("You're very hot, Guess a small bit higher");
+
+      else if( number > (this.numberToGuess + 2))
+          this.output("You're very hot, Guess a small bit lower");
+
+      else if( number === (this.numberToGuess - 1))
+          this.output("You're on fire!!! AHHHH, Call the fire department and also guess a washington higher");
+
+      else if( number === (this.numberToGuess + 1))
+          this.output("You're on fire!!! AHHHH, Call the fire department and also guess a washington lower");
+
   }
 }
+
+
 //Validate inputs that they are real numbers between 1-100.
-game.checkIfNumber = function(number) {
-  if(typeof number !== "number") {
-    //output: "Please enter a valid number"
-    //return false;
-  } else if(number < 1 || number > 100) {
-    //output: "Please enter a number between 1-100"
-    //return false;
-  }
-  else {
+Game.prototype.checkIfNumber = function(number) {
+  if(typeof(number) === "number" && number >=1 || number <=100) {
     return true;
-}
+  } else {
+    this.output("Please enter a number between 1 and 100");
+    return false;
+  }
+};
 //Create a button that provides the answer (Give me a Hint).
-game.giveAHint = function() {
-  //output: game.numberToGuess;
-  game.numberOfGuesses = 0;
-}
+Game.prototype.giveAnswer = function() {
+    this.output("Answer was: " + this.numberToGuess);
+    this.numberOfGuesses = 0;
+};
 
-game.previousAnswers = function() {
-  //output: <ol><li>game.guesses[]</li></ol>
-}
-
-
-game.guess = function(number) {
-  if(games.turnsLeft) {
-    if($.inArray(number, game.guesses) > 0) {
-      if(game.checkNumber(number));
-        game.guesses.push(number);
-        game.previousAnswers();
-        game.numberOfGuesses --;
+Game.prototype.guess = function(number) {
+  if(this.turnsLeft()) {
+    if(!($.inArray(number, this.guesses) > 0)) {
+      if(this.checkIfNumber(number)) {
+        number = parseInt(number);
+        this.numberOfGuesses --;
+        this.guesses.push(number);
+        this.checkGuess(number);
+      }
     } else {
-      //output: you already guessed that number;
+      this.output("You have already guessed that number");
     }
   }
-}
+};
 
+var game = new Game();
+game.output("Guess a number between 1 - 100");
 
+$("#newGame").click(function() {
+  game = new Game();
+  game.output("New Game");
+});
 
-//The user should have an input field where they can submit a guess.
+$("#submitInput").click(function() {
+  if($("#numberInput").val() === "") {
+    alert("You must enter something to guess!");
+  } else {
+    game.guess($("#numberInput").val());
+    $("#numberInput").val("");
+  }
+});
 
-//Create a new game button that resets the game.
-
-//Submit the guess by pressing enter or clicking the submit button.
-
-//Change the background color, add an image, or do something creative when the user guesses the correct answer.
+$("#answer").click(function() {
+  game.giveAnswer();
+});
